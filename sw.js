1523f7c3e6ac537a34491ced0094094234f6e5d9
henrykiarie04-cloud <henrykiarie04@gmail.com>
2026-09-22 03:36:32 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const C = "new-era-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(C).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
